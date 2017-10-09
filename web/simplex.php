fefb4920c787ad7587439427c6f8b745c55f32b6
HannaMalaquias <hanna.malaquias@gmail.com>
2017-10-09 19:31:09 -0300

    Correcao de identacao e atualizacao de comentário

--- web/simplex.php
+++ web/simplex.php
@@ -1,74 +1,64 @@
 <html>
-<head>
-  <!--Import Google Icon Font-->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <!--Import materialize.css-->
-  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	<head>
+		<!--Import Google Icon Font-->
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<!--Import materialize.css-->
+		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
-  <!--Let browser know website is optimized for mobile-->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title></title>
-</head>
-<body>
-	<div id = "form">
-		<form action="simplex.jsp" method="post">
-			<h1 align = "center"><label for="simplex" align= "center">Simplex</label></h1>
+		<!--Let browser know website is optimized for mobile-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<title></title>
+	</head>
+	
+	<body>
+		<div id = "form">
+			<form action="simplex.jsp" method="post">
+				<h1 align = "center"><label for="simplex" align= "center">Simplex</label></h1>
 
-			<br>Função objetivo (FO): 
-				<?php
-					$fo = $_POST['fo'];
-					$i = 1;
-					echo "<input type = 'text' name = '".$i."' id = '".$i."' size = '4' required = 'true'>";
-					echo "x".$i;
-					while($i < $fo){
-						echo " + ";
+				<br>Função objetivo (FO): 
+					<?php
+						$fo = $_POST['fo']; //numero de variaveis da funcao objetiva
+						$i = 1;
 						echo "<input type = 'text' name = '".$i."' id = '".$i."' size = '4' required = 'true'>";
-						echo "x".++$i."";	
-					}
-				?>
-				
-			<br><br><br>Restrições: <br>			
-				<?php
-					$restricoes = $_POST['restricoes'];
-					$tamRestricoes = $_POST['tamRestricoes'];
-
-					$i = 1;
-					$cont = 0;
-
-					while($i <= $restricoes){
-						echo "<br><br><input type = 'text' name = '".$i."' id = '".$i."' size = '4' required = 'true'>";
-						echo "x".++$cont;
-						$j = 1;
-						while($j < $tamRestricoes){
+						echo "x".$i;
+						
+						while($i < $fo){ //cria campos de variaveis para a funcao objetiva
 							echo " + ";
 							echo "<input type = 'text' name = '".$i."' id = '".$i."' size = '4' required = 'true'>";
-							echo "x".++$cont."";
-						$j++;	
+							echo "x".++$i."";	
 						}
-						$i++;
-					}
-				?>           
-			</div>
-			
-			<br> <input type = "submit" name = "button" value = "Executar Simplex" id = "button">		
-			
-	</div>
-          
-<p>
-    <!--Import jQuery before materialize.js-->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="js/materialize.min.js"></script>
-    
-            <script type ="text/javascript">
-    
-			function mostrar(){
-				document.getElementById('divRestricoes').hidden = false;
+					?>
 				
-				var nome = document.getElementById('nome').value;
+				<br><br><br>Restrições:			
+					<?php
+						$restricoes = $_POST['restricoes']; //numero de restricoes
+						$tamRestricoes = $_POST['tamRestricoes']; //tamanho padrao das restricoes
 
-			}
+						$i = 1;
+						$cont = 0;
+
+						while($i <= $restricoes){ //cria campos para as restricoes
+							echo "<br><br><input type = 'text' name = '".$i."' id = '".$i."' size = '4' required = 'true'>";
+							echo "x".++$cont;
+							$j = 1;
+							
+							while($j < $tamRestricoes){ // cria campos de variaveis para as restricoes
+								echo " + ";
+								echo "<input type = 'text' name = '".$i."' id = '".$i."' size = '4' required = 'true'>";
+								echo "x".++$cont."";
+								$j++;	
+							}
+							
+							$i++;
+						}
+					?>           
 			
-			
-    </script>
-</body>
+				<br><br> <input type = "submit" name = "button" value = "Executar Simplex" id = "button">		
+			</form>	
+		</div>
+          
+    		<!--Import jQuery before materialize.js-->
+    		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    		<script type="text/javascript" src="js/materialize.min.js"></script>						
+	</body>
 </html>
