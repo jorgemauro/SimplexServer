5d1ce10d529d286fcaabe2e8bbb4230bea7d2ad6
HannaMalaquias <hanna.malaquias@gmail.com>
2017-10-09 23:18:45 -0300

    Atualização de comentários

--- src/main/java/com/simplexservice/control/SimplexExec.java
+++ src/main/java/com/simplexservice/control/SimplexExec.java
@@ -4,40 +4,60 @@ import com.simplexservice.control.util.Change;
 
 import java.math.BigDecimal;
 
+/**
+ * Execucao do Simplex.
+ * */
 public class SimplexExec {
     Fases T;
 
-    public SimplexExec(){
+
+	/**
+	 * Construtor padrao que inicializa o atributo T do tipo Fases.
+	 * */		
+	public SimplexExec(){
         this.T=new Fases();
     }
 
+
+	/**
+	 * Inicializa a execucao do Simplex.
+	 * @param T - Objeto do tipo Fases a ser processado.
+	 * */
     public static void ExcFirst(Fases T){
         int colAccept=0;
         int lineAccept=0;
         int TheLine=0;
 
-                lineAccept=T.firstDotOne();
-                if(lineAccept>0){
-                    colAccept=T.firstDotTwo(lineAccept);
-                    if(colAccept>0){
-                        TheLine=T.firstDotThree(colAccept);
-                        System.out.println("fim primeira fase");
-                        Change.PrintTable(T);
-                        System.out.println("--------------------------------------------------------------------");
+		lineAccept=T.firstDotOne();
+		if(lineAccept>0){
+			colAccept=T.firstDotTwo(lineAccept);
+			if(colAccept>0){
+				TheLine=T.firstDotThree(colAccept);
+				System.out.println("fim primeira fase");
+				Change.PrintTable(T);
+				System.out.println("--------------------------------------------------------------------");
 
-                        SimplexExec.ChangeExec(TheLine,colAccept,T);
+				SimplexExec.ChangeExec(TheLine,colAccept,T);
 
-                    }else{
-                        System.out.println("Sem solução");
-                        Change.PrintTable(T);
-                        System.out.println("--------------------------------------------------------------------");
-                        System.out.println("Sem solução");
-                    }
-                }else{
-                    SimplexExec.ExcSecond(T);
-                }
+			}else{
+				System.out.println("Sem solução");
+				Change.PrintTable(T);
+				System.out.println("--------------------------------------------------------------------");
+				System.out.println("Sem solução");
+			}
+		}else{
+			SimplexExec.ExcSecond(T);
+		}
 
     }
+    
+    
+    /**
+     * Execucao da primeira fase.
+     * @param TheLine - inteiro indicando a linha a ser processada.
+     * @param colAccept - inteiro indicando a coluna.
+	 * @param T - Objeto do tipo Fases a ser manipulado. 
+     * */
     public static void ChangeExec(int TheLine,int colAccept, Fases T ){
         Fases NewT;
         Cell cellAccept=T.getMatrix()[TheLine][colAccept];
@@ -57,9 +77,13 @@ public class SimplexExec {
         }else {
             ExcSecond(NewT);
         }
-
     }
 
+
+	/**
+	 * Execucao da segunda fase.
+	 * @param T - Objeto do tipo Fases a ser manipulado.
+	 * */
     private static void ExcSecond(Fases T) {
         int colAccept=0;
         int lineAccept=0;
